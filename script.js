684692ccf5b7053de7b206aa8a4d2a599b7ab8c5
Damith Dilhan <55306956+DamithDilhan@users.noreply.github.com>
2021-07-22 12:43:09 +0530

    refactor code

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    if (p == 0){
+        return false;
+    }
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    result = document.getElementById('result')
+    var amount  = (p*(rate/100)*years) ;
+    var today = new Date().getFullYear() + Number(years) ;
     
+    result.innerHTML = "If you deposit <mark>"+ p +"</mark>,<br>";
+    result.innerHTML +="at an interest rate of <mark>"+ rate +"%</mark>,<br>";
+    result.innerHTML +="You will receive amount of <mark>" + Number(amount).toFixed(2) +"</mark>,<br>"
+    result.innerHTML +="in the year <mark>" +today+"</mark>";
+
+    
+}
+function update(value)
+{
+    document.getElementById("rate_label").innerHTML = value + "%";
 }
         
